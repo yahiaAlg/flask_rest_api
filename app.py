c94ec63e5cd21e49ebf65fa6c2d1b0118b401165
yahiaAlg <yahialinus21alg@@gmail.com>
2024-08-31 15:52:59 +0100

    fixing the functional programmed api

--- app.py
+++ app.py
@@ -8,16 +8,11 @@ stores = {}
 items = {}
 
 
-@app.get("/stores")
-def get_stores():
-    return list(stores.values())
-
-
 # @app.post("/store")
 # def create_store():
 #     new_store: dict = request.get_json()
 #     pprint(new_store)
-#     new_store.update({"items": []})
+#     new_store.update({items": [])
 #     stores.append(new_store)
 #     return stores, 201
 
@@ -30,7 +25,7 @@ def get_stores():
 #             store["items"].append(new_item)
 #             return store, 201
 #         else:
-# abort(404,             return "Store not found"})
+# abort(404,            return "Store not found")
 
 
 # @app.get("/store/<string:name>")
@@ -38,7 +33,7 @@ def get_stores():
 #     for store in stores:
 #         if store["name"] == name:
 #             return store, 200
-# abort(404,     return "Store not found"})
+# abort(404,    return "Store not found")
 
 
 # @app.get("/store/<string:name>/item")
@@ -46,49 +41,39 @@ def get_stores():
 #     for store in stores:
 #         if store["name"] == name:
 #             return store["items"], 200
-# abort(404,     return "Store not found"})
+# abort(404,    return "Store not found")
+@app.get("/stores")
+def get_stores():
+    return list(stores.values())
 
 
 @app.get("/store")
 def get_store():
     store_data = request.get_json()
-    store_id = store_data["id"]
+    store_id = store_data["store_id"]
     if store_id in stores:
         return stores[store_id]
-    return abort(404, {"message": f"no store with an id of {store_id}"})
+    return abort(404, message=f"no store with an id of {store_id}")
 
 
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
     store_id = uuid.uuid4().hex
-    new_store = {**store_data, "id": store_id}
+    new_store = {**store_data, "store_id": store_id}
     stores[store_id] = new_store
     return new_store, 201
 
 
-@app.get("/items")
-def get_items():
-    return list(items.values())
-
-
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        return abort(404, {"message": "item not found"})
-
-
 @app.put("/store")
-def update_store(store_id):
+def update_store():
     store_id = request.json["store_id"]
     if store_id not in stores:
-        return abort(404, ({"message": f"no such a store with an id of {store_id}"}))
-    required_fields = ["name", "address"]
+        return abort(404, message=f"no such a store with an id of {store_id}")
+    required_fields = ["name", "store_id"]
     store_data = request.get_json()
     if not all(field in store_data for field in required_fields):
-        return abort(400, "missing required fields")
+        return abort(400, message="missing required fields")
     stores[store_id] |= store_data
     return stores[store_id], 201
 
@@ -102,8 +87,21 @@ def delete_store():
     return f"store with id {store_id} has been deleted", 200
 
 
+@app.get("/items")
+def get_items():
+    return list(items.values())
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return abort(404, message="item not found")
+
+
 @app.post("/item")
-def create_items():
+def create_item():
     required_fields = [
         "name",
         "price",
@@ -113,12 +111,12 @@ def create_items():
     ]
     item_data = request.get_json()
     if not all(field in item_data for field in required_fields):
-        return abort(400, "missing required fields")
+        return abort(400, massage="missing required fields")
     store_id = item_data["store_id"]
     if store_id not in stores:
-        return abort(404, {"message": f"no such a store with an id of {store_id}"})
+        return abort(404, message=f"no such a store with an id of {store_id}")
     item_id = uuid.uuid4().hex
-    new_item = {**item_data, "id": item_id}
+    new_item = {**item_data, "item_id": item_id}
     items[item_id] = new_item
     return new_item, 201
 
@@ -126,8 +124,8 @@ def create_items():
 @app.put("/item/<string:id>")
 def update_item(id):
     if id not in items:
-        return abort(404, {"message": f"no item with an id of {id}"})
-    required_fields = [
+        return abort(404, message=f"no item with an id of {id}")
+    permitted_fields = [
         "name",
         "price",
         "store_id",
@@ -135,8 +133,8 @@ def update_item(id):
         "category",
     ]
     item_data = request.get_json()
-    if not all(field in item_data for field in required_fields):
-        return abort(400, "missing required fields")
+    if not all(field in permitted_fields for field in item_data):
+        return abort(400, message="not permitted fields")
     items[id] |= item_data
     return items[id], 201
 
@@ -144,6 +142,6 @@ def update_item(id):
 @app.delete("/item/<string:id>")
 def delete_item(id):
     if id not in items:
-        return abort(404, {"message": f"no item with an id of {id}"})
+        return abort(404, message=f"no item with an id of {id}")
     del items[id]
     return f"item with id {id} has been deleted", 200
